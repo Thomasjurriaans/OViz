51f052f9946f5d11cc5c735b4e421adbca781732
Thomas Jurriaans <20623697+Thomasjurriaans@users.noreply.github.com>
2022-01-30 23:48:57 +0100

    Updated APIconfiguration to match NS' changes

--- APIconfig.py
+++ APIconfig.py
@@ -1,7 +1,3 @@
-# This is the key used for the "getTrips" API
-public_travel_information_key = "your key here"
-
-
-# This is the key used for the "getSpoorkaart" API
-ns_app_key = "other key here"
+# This is the key used for the NS Api's, use the primary key for "Ns-App"
+ns_app_key = "enter key here without whitespace"
 
